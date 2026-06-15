f437b6732661adf8a5d49227be2793cfde827fe3
Sven-Milicevic <sven.milicevic@edu.sait.ca>
2026-06-14 22:48:26 -0600

    Fix Azurite connection string for SDK 12.19.0 compatibility #2

--- function.py
+++ function.py
@@ -6,7 +6,7 @@ import json
 
 CONNECTION_STRING = os.environ.get(
     "AZURE_STORAGE_CONNECTION_STRING",
-    "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6ImpFQjSjbZ+pM3hq9NAdnhN+Q==;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;"
+    "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;"
 )
 
 def process_nutritional_data_from_azurite():
--- upload_blob.py
+++ upload_blob.py
@@ -3,7 +3,7 @@ from azure.storage.blob import BlobServiceClient
 
 connection_string = os.environ.get(
     "AZURE_STORAGE_CONNECTION_STRING",
-    "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6ImpFQjSjbZ+pM3hq9NAdnhN+Q==;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;"
+    "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;"
 )
 
 blob_service_client = BlobServiceClient.from_connection_string(connection_string)
